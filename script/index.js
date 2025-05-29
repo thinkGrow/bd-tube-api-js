38d324f75a9a69de86f4f56492923a77db3e6b12
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-29 12:36:35 +0600

    api loading mechanism for categories

--- script/index.js
+++ script/index.js
@@ -1,13 +1,30 @@
 function loadCategories() {
   // fetch the data
-
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    // convert promise to json
     .then((response) => response.json())
-    .then((data) => displayCategories(data));
+    // send data to display
+    .then((data) => displayCategories(data.categories));
 }
 
-function displayCategories(data){
-     console.log(data.categories);
+function displayCategories(categories) {
+  //get the container
+  const categoryContainer = document.getElementById("category-container");
+  //Loop operation on Array of object
+  for (let cat of categories) {
+    console.log(cat);
+
+    //create element
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+    
+    `;
+
+    categoryContainer.appendChild(categoryDiv);
+  }
+
+  //append element
 }
 
 loadCategories();
