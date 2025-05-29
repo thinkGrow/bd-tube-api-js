0152ff0814167c8f6afd9814d7c6a14ee8faf4f2
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-29 14:47:02 +0600

    show videos dynamically - style

--- script/index.js
+++ script/index.js
@@ -34,26 +34,80 @@ function loadVideos() {
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
+
+  videoContainer.innerHTML = "";
+
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+    <div
+        class="py-20 col-span-full flex flex-col justify-center items-center text-center"
+      >
+        <img class="w-[120px]" src="./assets/Icon.png" alt="" />
+        <h2 class="text-2xl font-bold">
+          Oops!! Sorry, There is no content here
+        </h2>
+      </div>
+    `;
+    hideLoader();
+    return;
+  }
+
   videos.forEach((video) => {
     // console.log(video);
-    const videoDiv = document.createElement("div");
-    videoDiv.innerHTML = `
-        <div class="card bg-base-100 shadow-sm">
-            <figure>
-                <img src="${video.thumbnail}" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">${video.title}</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
-        `;
 
-    videoContainer.append(videoDiv);
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
+     <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${
+            video.thumbnail
+          }" alt="Shoes" />
+          <span
+            class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2"
+            >3hrs 56 min ago</span
+          >
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+             ${video.authors[0].profile_name}
+              ${
+                video.authors[0].verified == true
+                  ? `<img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />`
+                  : ``
+              }
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+
+        </div>
+
+      </div>
+    
+    `;
+    //append
+    videoContainer.append(videoCard);
   });
+  hideLoader();
 };
 
 loadVideos();
